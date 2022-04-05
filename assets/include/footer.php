cd75835efcd2ffb7b33ef5bfdba2a12591cd5816
mxmlgff <92386258+mxmlgff@users.noreply.github.com>
2022-04-05 20:32:18 +0200

    Add files via upload

---
+++ assets/include/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+        <div class="para-footer max-width">
+            <p class="text-footer t1">Rapport de Stage</p>
+            <p class="text-footer">Réalisé par Maxime LE GOFF</p>
+            <p class="text-footer">Dans le cadre de la deuxième année de <span><strong>DEUST Webmaster & Métiers de l'internet</strong></span></p>
+        </div>
+</footer>
---
+++ assets/include/menu.php
@@ -0,0 +1,11 @@
+<div class="menu-include">
+               
+    <nav class="menu-header-include">
+        <ul class="elements-menu">
+            <li class="details-link"><a id="index" href="../index.php">Présentation</a></li>
+            <li class="details-link"><a id="entreprise" href="rapport-lancement.php">Rapport de lancement</a></li>
+            <li class="details-link"><a id="stage" href="rapport-final.php">Rapport final</a></li>
+        </ul>
+    </nav>
+
+</div>
